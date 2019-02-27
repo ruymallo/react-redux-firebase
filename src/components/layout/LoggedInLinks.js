08e3959acdb53e7ab59ba4cc04ec3fae3aed2589
Ruy Mallo <ruymallo@gmail.com>
2019-02-26 23:19:26 -0300

    Create LoggedInLinks component and add it to Navbar

---
+++ src/components/layout/LoggedInLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const LoggedInLinks = () => {
+  return (
+    <ul className="right">
+      <li><NavLink to="/" >New Project</NavLink> </li>
+      <li><NavLink to="/" >Log Out</NavLink> </li>
+      <li><NavLink to="/" className="btn btn-floating pink" >RUY</NavLink> </li>
+    </ul>
+  );
+}
+
+export default LoggedInLinks;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoggedInLinks from './LoggedInLinks'
 
 const Navbar = () => {
   return(
     <nav className="nav-wrapper grey darken-3" >
       <div className="container">
         <Link to="/" className="brand-logo" >MarioPlan</Link>
+        <LoggedInLinks />
       </div>
     </nav>
   );
