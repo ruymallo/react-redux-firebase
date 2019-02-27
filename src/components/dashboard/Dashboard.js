ae8e340053e0859573c7869d62d98749a91b3d6d
Ruy Mallo <ruymallo@gmail.com>
2019-02-26 23:31:38 -0300

    Import ProjectList and Notifications to Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-class Dashboard extends React.Component {
+import Notifications from './Notifications';
+import ProjectList from '../projects/ProjectList';
+
+export default class Dashboard extends React.Component {
   render() {
     return(
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            
+            <ProjectList />
           </div>
           <div className="col s12 m5 offset-m1">
-          
+            <Notifications />
           </div>
         </div>
       </div>
     );
   }
 }
+
