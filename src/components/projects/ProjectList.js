3a041f74f548a530dfda418b8c496fe6b9532cab
Ruy Mallo <ruymallo@gmail.com>
2019-02-26 23:35:50 -0300

    Create dummy projects in ProjectList component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -10,6 +10,22 @@ export default function ProjectList() {
           <p className="grey-text">12/12/19</p>
         </div>
       </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Ruy</p>
+          <p className="grey-text">12/12/19</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Ruy</p>
+          <p className="grey-text">12/12/19</p>
+        </div>
+      </div>
     </div>
   );
 }
