54513e605c91783b06469e1f12fc0472d7f5bed7
Ruy Mallo <ruymallo@gmail.com>
2019-02-26 23:25:07 -0300

    Create skeletton of Dashboard component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return(
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            
+          </div>
+          <div className="col s12 m5 offset-m1">
+          
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
