06d055ac829d87085f77f5682821d9a4b7737231
Ruy Mallo <ruymallo@gmail.com>
2019-02-26 23:36:16 -0300

    Add Dashboard to App inside Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
