80bfebf862856509ee0fea41bf5d6608279bdeee
Ruy Mallo <ruymallo@gmail.com>
2019-02-27 22:38:05 -0300

    Add routes for SignUp and LogIn in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import LogIn from './components/auth/LogIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends React.Component {
   render() {
@@ -14,6 +16,8 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
