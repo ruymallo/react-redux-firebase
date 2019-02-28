b5f210a847a10fd359a02025abc7cec1edb105f3
Ruy Mallo <ruymallo@gmail.com>
2019-02-27 22:07:16 -0300

    Add ProjectDetails to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 class App extends React.Component {
   render() {
@@ -11,7 +12,8 @@ class App extends React.Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
