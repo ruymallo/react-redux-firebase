99b80d6f88fb290af637b4fc8e4feec236db5dc6
Ruy Mallo <ruymallo@gmail.com>
2019-02-27 21:38:29 -0300

    Add ProjectSumary to ProjectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,28 +1,14 @@
 import React from 'react';
 
+import ProjectSumary from './ProjectSumary'
+
 export default function ProjectList() {
   return (
     <div className="project-list section" >
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ruy</p>
-          <p className="grey-text">12/12/19</p>
-        </div>
-
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ruy</p>
-          <p className="grey-text">12/12/19</p>
-        </div>
-
-        <div className="card-content">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ruy</p>
-          <p className="grey-text">12/12/19</p>
-        </div>
-      </div>
-
+      <ProjectSumary />
+      <ProjectSumary />
+      <ProjectSumary />
+      <ProjectSumary />
     </div>
   );
 }
