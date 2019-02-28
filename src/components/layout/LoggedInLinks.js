3dc8276a968f9736e68ca29146bb5d116dbb2abc
Ruy Mallo <ruymallo@gmail.com>
2019-02-27 23:16:08 -0300

    change Navlink in LoggedinLinks to go to create route

--- src/components/layout/LoggedInLinks.js
+++ src/components/layout/LoggedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const LoggedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/" >New Project</NavLink> </li>
+      <li><NavLink to="/create" >New Project</NavLink> </li>
       <li><NavLink to="/" >Log Out</NavLink> </li>
       <li><NavLink to="/" className="btn btn-floating pink" >RUY</NavLink> </li>
     </ul>
