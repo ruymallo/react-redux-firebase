30f8d5105d49cb78810e832204d7afd4f6a3261d
Ruy Mallo <ruymallo@gmail.com>
2019-02-27 21:37:54 -0300

    Create ProjectSumary component

---
+++ src/components/projects/ProjectSumary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ProjectSumary() {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Ruy</p>
+        <p className="grey-text">12/12/19</p>
+      </div>
+    </div>
+  );
+}
