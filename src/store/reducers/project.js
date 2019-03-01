99289f717fd71a7360b7c7995481bc408002f5b0
Ruy Mallo <ruymallo@gmail.com>
2019-03-01 00:20:18 -0300

    Handle action type CREATE_PROJECT_SUCCESS in projects reducer

--- src/store/reducers/project.js
+++ src/store/reducers/project.js
@@ -1,12 +1,19 @@
 import { combineReducers } from "redux";
+import concat from 'lodash/concat';
 
-const projectsInitialState =[
+import { CREATE_PROJECT_SUCCESS } from '../constants/actionTypes';
+
+const projectsInitialState = [
   { id: '1', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
   { id: '2', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
   { id: '3', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' }
 ];
 
-function projects(state = projectsInitialState, action) {
+function projects(state = projectsInitialState, action = {}) {
+  if (action.type === CREATE_PROJECT_SUCCESS) {
+    return concat(state, action.project);
+  }
+  
   return state;
 }
 
