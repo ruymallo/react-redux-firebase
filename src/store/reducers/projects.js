dbabd531eb4e324ecba951665a0a296c8a06c1ca
Ruy Mallo <ruymallo@gmail.com>
2019-02-28 21:35:01 -0300

    Refactor projects reducer to export as default combinedreducers

--- src/store/reducers/projects.js
+++ src/store/reducers/projects.js
@@ -1,11 +1,15 @@
-const initialState = {
-  projects: [
-    { id: '1', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
-    { id: '2', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
-    { id: '3', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' }
-  ]
-};
+import { combineReducers } from "redux";
 
-export default function projects(state = initialState, action) {
+const projectsInitialState =[
+  { id: '1', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
+  { id: '2', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
+  { id: '3', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' }
+];
+
+function projects(state = projectsInitialState, action) {
   return state;
 }
+
+export default combineReducers({
+  projects
+});
