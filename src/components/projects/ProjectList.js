c90b99336c45170b177184d1a09e84568dc27363
Ruy Mallo <ruymallo@gmail.com>
2019-02-28 21:40:07 -0300

    Remove console.log from ProjectList component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ProjectSumary from './ProjectSumary'
 
 export default function ProjectList({ projects }) {
-  console.log(projects)
   const projectSummaries = projects.map(project => {
     return <ProjectSumary key={project.id} project={project} />;
   });
