9f6c7aefc1ffd7f6ca3de31fa2c0e3a067fbe1a7
Ruy Mallo <ruymallo@gmail.com>
2019-02-28 21:39:03 -0300

    Remove dummy components with rendered components by received props

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import ProjectSumary from './ProjectSumary'
 
-export default function ProjectList() {
+export default function ProjectList({ projects }) {
+  console.log(projects)
+  const projectSummaries = projects.map(project => {
+    return <ProjectSumary key={project.id} project={project} />;
+  });
+
   return (
     <div className="project-list section" >
-      <ProjectSumary />
-      <ProjectSumary />
-      <ProjectSumary />
-      <ProjectSumary />
+      { projectSummaries }
     </div>
   );
 }
