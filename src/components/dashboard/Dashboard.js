f0a39501dc2c3c814ee8a3c656a35487625c0f96
Ruy Mallo <ruymallo@gmail.com>
2019-02-28 21:38:40 -0300

    Connect Dashboard component and pass projects as prop to ProjectList

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { getProjects } from '../../store/selectors/project';
 
 import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 
-export default class Dashboard extends React.Component {
+class Dashboard extends React.Component {
   render() {
+    const projects = this.props.projects;
+
     return(
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects}  />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -20,3 +25,8 @@ export default class Dashboard extends React.Component {
   }
 }
 
+const mapStateToProps = state =>  ({
+  projects: getProjects(state)
+});
+
+export default connect(mapStateToProps)(Dashboard)
