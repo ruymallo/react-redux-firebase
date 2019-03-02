86576c036b86d85b26a11cd3ff95c6225f6f13d9
Ruy Mallo <ruymallo@gmail.com>
2019-03-02 00:11:29 -0300

    Import and implementation of addDocumentToFireStore actionCreator in project action

--- src/store/actions/project.js
+++ src/store/actions/project.js
@@ -1,12 +1,16 @@
+import { addDocumentToFiresore } from '../../utilities/firestoreActionCreators';
 import { CREATE_PROJECT_SUCCESS } from '../constants/actionTypes';
-
-export const createProject = project => {
-  return dispatch => {
-    dispatch(createProjectSuccess(project));
-  }
-};
+import { CREATE_PROJECT_ID } from '../constants/requestsIds';
 
 const createProjectSuccess = project => ({
   type: CREATE_PROJECT_SUCCESS,
   project
 });
+
+export const createProject = addDocumentToFiresore({
+  collection: 'projects',
+  requestId: CREATE_PROJECT_ID,
+  successCallback: createProjectSuccess,
+  errorCallback: console.log
+});
+
