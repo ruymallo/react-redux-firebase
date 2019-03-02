51a3448eab17257047cb9113623d86f6efc3a6c4
Ruy Mallo <ruymallo@gmail.com>
2019-03-02 02:27:19 -0300

    Create action to handle CREATE_PROJECT_ERROR action

--- src/store/actions/project.js
+++ src/store/actions/project.js
@@ -1,5 +1,5 @@
 import { addDocumentToFiresore } from '../../utilities/firestoreActionCreators';
-import { CREATE_PROJECT_SUCCESS } from '../constants/actionTypes';
+import { CREATE_PROJECT_SUCCESS, CREATE_PROJECT_ERROR } from '../constants/actionTypes';
 import { CREATE_PROJECT_ID } from '../constants/requestsIds';
 
 const createProjectSuccess = project => ({
@@ -7,10 +7,26 @@ const createProjectSuccess = project => ({
   project
 });
 
-export const createProject = addDocumentToFiresore({
+const createProjectError = error => ({
+  type: CREATE_PROJECT_ERROR,
+  error
+});
+
+const addProjectMetadata = callback => project => {
+  const finalObject = {
+    ...project,
+    createdAt: Date.now()
+  };
+
+  return callback(finalObject);
+};
+
+
+const addProjectToFirestore = addProjectMetadata(addDocumentToFiresore({
   collection: 'projects',
   requestId: CREATE_PROJECT_ID,
   successCallback: createProjectSuccess,
-  errorCallback: console.log
-});
+  errorCallback: createProjectError
+}));
 
+export const createProject = addProjectToFirestore
