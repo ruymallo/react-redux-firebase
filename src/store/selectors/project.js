291e226dedd52f4bd14b0fadb5c3faa1165a02c4
Ruy Mallo <ruymallo@gmail.com>
2019-03-18 22:11:22 -0300

    Add getFirestoreProjects selector

--- src/store/selectors/project.js
+++ src/store/selectors/project.js
@@ -1,3 +1,8 @@
 import get from 'lodash/fp/get';
+import getOr from 'lodash/fp/getOr';
 
 export const getProjects = get('project.projects');
+
+const defaultProjects = [];
+
+export const getFirestoreProjects = getOr(defaultProjects, 'firestore.ordered.projects')
