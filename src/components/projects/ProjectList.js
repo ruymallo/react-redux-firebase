bb19206eb5fe99731c428aa3473133709a73ff44
Ruy Mallo <ruymallo@gmail.com>
2019-03-18 22:45:03 -0300

    Add Link to ProjectDetails in ProjectList

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
 
 import { getFirestoreProjects } from '../../store/selectors/project'
 
 import ProjectSumary from './ProjectSumary'
 
-function ProjectList({ projects, firestore }) {
-  console.log(firestore);
+function ProjectList({ projects }) {
   
   const projectSummaries = projects.map(project => {
-    return <ProjectSumary key={project.id} project={project} />;
+    return (
+      <Link key={project.id} to={`/project/${project.id}`}>
+        <ProjectSumary project={project} />
+      </Link>
+    );
   });
 
   return (
