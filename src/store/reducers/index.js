24571998afb7c0eb93ca048a45db7c4cac4101e7
Ruy Mallo <ruymallo@gmail.com>
2019-03-18 22:11:01 -0300

    Add firestoreReducer to rootReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 import auth from './auth';
 import project from './project';
 
 export default combineReducers({
   auth,
+  firestore: firestoreReducer,
   project
 });
