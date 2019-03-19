53969c0715d230369f2fe5edcf2a38dddf0779f3
Ruy Mallo <ruymallo@gmail.com>
2019-03-19 00:22:13 -0300

    Handle action types LOGIN_SUCCESS and LOGIN_ERROR in auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,21 @@
+import { LOGIN_SUCCESS, LOGIN_ERROR } from '../constants/actionTypes';
+
 const initialState = {};
 
-export default function auth(state = initialState, action) {
+export default function auth(state = initialState, action = {}) {
+  if (action.type === LOGIN_SUCCESS) {
+    return {
+      ...state,
+      authError: null
+    };
+  }
+
+  if (action.type === LOGIN_ERROR) {
+    return {
+      ...state,
+      authError: action.error.message
+    };
+  }
+
   return state;
 }
