6a72d2ac4241d79b4f83eaaac5953faca8064857
Ruy Mallo <ruymallo@gmail.com>
2019-03-18 22:10:32 -0300

    Remove projects DUMMY data from project reducer

--- src/store/reducers/project.js
+++ src/store/reducers/project.js
@@ -3,13 +3,7 @@ import concat from 'lodash/concat';
 
 import { CREATE_PROJECT_SUCCESS, CREATE_PROJECT_ERROR } from '../constants/actionTypes';
 
-const projectsInitialState = [
-  { id: '1', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
-  { id: '2', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' },
-  { id: '3', title: 'Lorem, ipsum dolor.', content: 'Content lorem ipsum dolor sit.' }
-];
-
-function projects(state = projectsInitialState, action = {}) {
+function projects(state = [], action = {}) {
   if (action.type === CREATE_PROJECT_SUCCESS) {
     return concat(state, action.project);
   }
