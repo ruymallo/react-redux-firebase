9874398cc2f06dba0ee9c8974ea48bbf7654a7f7
Ruy Mallo <ruymallo@gmail.com>
2019-03-20 00:19:00 -0300

    Add email selector to LoggedInLinks

--- src/components/layout/LoggedInLinks.js
+++ src/components/layout/LoggedInLinks.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/auth';
+import { getUserEmail } from '../../store/selectors/auth';
 
-const LoggedInLinks = () => {
+const LoggedInLinks = ({ signOut, email }) => {
   return (
     <ul className="right">
       <li><NavLink to="/create" >New Project</NavLink> </li>
-      <li><NavLink to="/" >Log Out</NavLink> </li>
-      <li><NavLink to="/" className="btn btn-floating pink" >RUY</NavLink> </li>
+      <li><button onClick={signOut}>Log Out</button></li>
+      <li><NavLink to="/" className="btn btn-floating pink" >{email}</NavLink> </li>
     </ul>
   );
 }
 
-export default LoggedInLinks;
+const mapStateToProps = state => ({
+  email: getUserEmail(state)
+})
+
+const mapDispatchToProps = {
+  signOut
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoggedInLinks);
