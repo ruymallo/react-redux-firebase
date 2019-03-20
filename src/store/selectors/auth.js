e27a71f550597d4855e71e77dbdf9f4274f2ce49
Ruy Mallo <ruymallo@gmail.com>
2019-03-20 00:43:03 -0300

    Add isLoggedIn selector

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -1,8 +1,11 @@
 import get from 'lodash/fp/get';
 import pipe from 'lodash/fp/pipe';
+import has from 'lodash/fp/has';
 
 export const getAuthErrorMessage = get('auth.authError');
 
 export const getFirebase = get('firebase');
 
 export const getUserEmail = pipe(getFirebase, get('auth.email'));
+
+export const isLoggedIn = pipe(getFirebase, has('auth.uid'));
