d7f875397647a4a2c7c2b35d3fa4ec321f06965c
Ruy Mallo <ruymallo@gmail.com>
2019-03-20 00:18:35 -0300

    Create selector getUserEmail

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -1,5 +1,8 @@
 import get from 'lodash/fp/get';
+import pipe from 'lodash/fp/pipe';
 
 export const getAuthErrorMessage = get('auth.authError');
 
 export const getFirebase = get('firebase');
+
+export const getUserEmail = pipe(getFirebase, get('auth.email'));
