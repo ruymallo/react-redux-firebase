6e4b11f51b0961fed8bf5062154d9d4eb7ff24ed
Ruy Mallo <ruymallo@gmail.com>
2019-03-21 01:04:42 -0300

    Handle actions of types SIGN_UP_SUCCESS and SIGN_UP_ERROR

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,30 +1,52 @@
-import { LOGIN_SUCCESS, LOGIN_ERROR, SIGN_OUT_SUCCESS, SIGN_OUT_ERROR } from '../constants/actionTypes';
+import {
+  LOGIN_ERROR,
+  LOGIN_SUCCESS,
+  SIGN_OUT_ERROR,
+  SIGN_OUT_SUCCESS,
+  SIGN_UP_ERROR,
+  SIGN_UP_SUCCESS
+} from '../constants/actionTypes';
 
 const initialState = {};
 
 export default function auth(state = initialState, action = {}) {
-  if (action.type === LOGIN_SUCCESS) {
-    return {
-      ...state,
-      authError: null
-    };
-  }
+  switch (action.type) {
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        authError: action.error.message
+      };
+    
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        authError: null
+      };
+    
+    case SIGN_OUT_ERROR:
+      console.error(action.error);
 
-  if (action.type === LOGIN_ERROR) {
-    return {
-      ...state,
-      authError: action.error.message
-    };
-  }
+      return state;
 
-  if (action.type === SIGN_OUT_ERROR) {
-    console.error(action.error);
-  }
+    case SIGN_OUT_SUCCESS:
+      console.log(SIGN_OUT_SUCCESS);
+      
+      return state;
 
-  if (action.type === SIGN_OUT_SUCCESS) {
-    console.log('SIGN_OUT_SUCCESS');
-  }
-  
+    case SIGN_UP_ERROR:
+      return {
+        ...state,
+        authError: action.error.message
+      };
 
-  return state;
+
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        authError: null
+      };
+      
+    default:
+      return state
+  }
 }
