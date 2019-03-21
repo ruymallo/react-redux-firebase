2abaa4a71c7c4ac7b6e2c8ac139355effd0387d2
Ruy Mallo <ruymallo@gmail.com>
2019-03-21 01:33:45 -0300

    Add user initials LoggedInLinks

--- src/components/layout/LoggedInLinks.js
+++ src/components/layout/LoggedInLinks.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/auth';
-import { getUserEmail } from '../../store/selectors/auth';
+import { getUserInitials } from '../../store/selectors/auth';
 
 import { CREATE_ROUTE, HOME_ROUTE } from '../layout/constants';
 
-const LoggedInLinks = ({ signOut, email }) => {
+const LoggedInLinks = ({ signOut, userInitials }) => {
   return (
     <ul className="right">
       <li><NavLink to={CREATE_ROUTE} >New Project</NavLink> </li>
       <li><button onClick={signOut}>Log Out</button></li>
-      <li><NavLink to={HOME_ROUTE} className="btn btn-floating pink" >{email}</NavLink> </li>
+      <li><NavLink to={HOME_ROUTE} className="btn btn-floating pink" >{userInitials}</NavLink> </li>
     </ul>
   );
 }
 
 const mapStateToProps = state => ({
-  email: getUserEmail(state)
+  userInitials: getUserInitials(state)
 })
 
 const mapDispatchToProps = {
