75c36e1ed1c0fb373ee03e09eab4a3c80e5a8cb0
Ruy Mallo <ruymallo@gmail.com>
2019-03-21 01:05:36 -0300

    Add errorMessage and dispatching signUp to SignUp component

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { signUp } from '../../store/actions/auth';
+
 import { HOME_ROUTE } from '../layout/constants';
 import { Redirect } from 'react-router-dom';
-import { isLoggedIn } from '../../store/selectors/auth';
+import { isLoggedIn, getAuthErrorMessage } from '../../store/selectors/auth';
 
 
 class SingUp extends React.Component {
@@ -16,7 +18,8 @@ class SingUp extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
+
+    this.props.signUp(this.state);
   }
 
   handleChange = event => {
@@ -27,7 +30,7 @@ class SingUp extends React.Component {
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, errorMessage } = this.props;
     if (isLoggedIn) return <Redirect to={HOME_ROUTE} />;
     return (
       <div className="container">
@@ -49,21 +52,27 @@ class SingUp extends React.Component {
           </div>
 
           <div className="input-field">
-            <label htmlFor="LastName">LastName</label>
-            <input type="text" id="LastName" onChange={this.handleChange} />
+            <label htmlFor="lastName">lastName</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
           </div>
 
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">SingUp</button>
           </div>
         </form>
+        <p className="red-text">{errorMessage ? errorMessage : null}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  isLoggedIn: isLoggedIn(state)
-})
+  isLoggedIn: isLoggedIn(state),
+  errorMessage: getAuthErrorMessage(state),
+});
 
-export default connect(mapStateToProps)(SingUp);
+const mapDispatchToProps = {
+  signUp: signUp
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingUp);
