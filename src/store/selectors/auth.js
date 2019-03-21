ad88456311d535e21fbfe55a622345eaf4fa0334
Ruy Mallo <ruymallo@gmail.com>
2019-03-21 01:32:55 -0300

    Add auth selector getUserInitials

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -9,3 +9,12 @@ export const getFirebase = get('firebase');
 export const getUserEmail = pipe(getFirebase, get('auth.email'));
 
 export const isLoggedIn = pipe(getFirebase, has('auth.uid'));
+
+export const getUserInitials = (state) => {
+  const profile = get('firebase.profile')(state);
+  if (has('firstName')(profile) && has('lastName')(profile)) {
+    return profile.firstName[0] + profile.lastName[0];
+  }
+
+  return '';
+}
