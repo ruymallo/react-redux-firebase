b0b50ba0797f157ade5c5dc8669b9255a3fbd419
Ruy Mallo <ruymallo@gmail.com>
2019-03-20 23:32:44 -0300

    Replace routes with constants

--- src/components/layout/LoggedOutLinks.js
+++ src/components/layout/LoggedOutLinks.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { SIGNUP_ROUTE, LOGIN_ROUTE } from '../layout/constants';
+
+
 const LoggedOutLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/signup" >Sign Up</NavLink> </li>
-      <li><NavLink to="/login" >Log In</NavLink> </li>
+      <li><NavLink to={SIGNUP_ROUTE} >Sign Up</NavLink> </li>
+      <li><NavLink to={LOGIN_ROUTE} >Log In</NavLink> </li>
     </ul>
   );
 }
