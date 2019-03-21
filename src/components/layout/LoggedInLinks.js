ee1f1c8484eceff9659311c3c7cf0d576517dc0a
Ruy Mallo <ruymallo@gmail.com>
2019-03-20 23:32:28 -0300

    Replace routes with constants

--- src/components/layout/LoggedInLinks.js
+++ src/components/layout/LoggedInLinks.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/auth';
 import { getUserEmail } from '../../store/selectors/auth';
 
+import { CREATE_ROUTE, HOME_ROUTE } from '../layout/constants';
+
 const LoggedInLinks = ({ signOut, email }) => {
   return (
     <ul className="right">
-      <li><NavLink to="/create" >New Project</NavLink> </li>
+      <li><NavLink to={CREATE_ROUTE} >New Project</NavLink> </li>
       <li><button onClick={signOut}>Log Out</button></li>
-      <li><NavLink to="/" className="btn btn-floating pink" >{email}</NavLink> </li>
+      <li><NavLink to={HOME_ROUTE} className="btn btn-floating pink" >{email}</NavLink> </li>
     </ul>
   );
 }
