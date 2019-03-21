87b150caf72db4b78b533752eeab5bdbb2526cd2
Ruy Mallo <ruymallo@gmail.com>
2019-03-21 01:04:13 -0300

    add actionCreators for SIGN_UP_SUCCESS and SIGN_UP_ERROR

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,14 @@
-import { LOGIN_SUCCESS, LOGIN_ERROR, SIGN_OUT_SUCCESS, SIGN_OUT_ERROR } from '../constants/actionTypes';
+import {
+  LOGIN_ERROR,
+  LOGIN_SUCCESS,
+  SIGN_OUT_ERROR,
+  SIGN_OUT_SUCCESS,
+  SIGN_UP_ERROR,
+  SIGN_UP_SUCCESS
+} from '../constants/actionTypes';
 
 export const logIn = credentials => {
-  return (dispatch, getState, { getFirebase }) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firebase = getFirebase();
 
     firebase.auth().signInWithEmailAndPassword(
@@ -26,7 +33,6 @@ const logInSuccess = token => ({
 
 export const signOut = () => {
   return (dispatch, getState, {getFirebase}) => {
-    window.getState = getState;
     const firebase = getFirebase();
 
     firebase.auth().signOut()
@@ -43,3 +49,33 @@ const signOutError = error => ({
   type: SIGN_OUT_ERROR,
   error
 });
+
+const signUpError = error => ({
+  type: SIGN_UP_ERROR,
+  error
+});
+
+const signUpSuccess = () => ({
+  type: SIGN_UP_SUCCESS
+});
+
+
+export const signUp = user => {
+  return (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+
+    firebase.auth().createUserWithEmailAndPassword(
+      user.email, user.password
+    )
+    .then(firebaseResponse => firestore
+      .collection('users')
+      .doc(firebaseResponse.user.uid)
+      .set({
+        firstName: user.firstName,
+        lastName: user.lastName
+      })
+    ).then(() => dispatch(signUpSuccess()))
+    .catch(error => dispatch(signUpError(error)))
+  }
+}
