ebad239dd29ec43f8eae2f5e199b8a2a1a944814
Ruy Mallo <ruymallo@gmail.com>
2019-03-23 15:35:17 -0300

    Add and refactor auth selectors

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -2,16 +2,23 @@ import get from 'lodash/fp/get';
 import pipe from 'lodash/fp/pipe';
 import has from 'lodash/fp/has';
 
-export const getAuthErrorMessage = get('auth.authError');
-
 export const getFirebase = get('firebase');
 
-export const getUserEmail = pipe(getFirebase, get('auth.email'));
+export const getAuth = pipe(getFirebase, get('auth'));
 
-export const isLoggedIn = pipe(getFirebase, has('auth.uid'));
+export const getAuthErrorMessage = pipe(getAuth, get('authError'));
+
+export const getProfile = pipe(getFirebase, get('profile'));
+
+export const getUserEmail = pipe(getAuth, get('email'));
+
+export const isLoggedIn = pipe(getAuth, has('uid'));
+
+export const getUid = pipe(getAuth, get('uid'));
 
 export const getUserInitials = (state) => {
-  const profile = get('firebase.profile')(state);
+  const profile = getProfile(state);
+
   if (has('firstName')(profile) && has('lastName')(profile)) {
     return profile.firstName[0] + profile.lastName[0];
   }
