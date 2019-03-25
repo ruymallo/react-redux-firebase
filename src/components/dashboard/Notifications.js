0b1c58b4e46c1db2da35e8a67a6e753cbe7b888f
Ruy Mallo <ruymallo@gmail.com>
2019-03-24 22:11:17 -0300

    Connect Notifications component to firestore and redux

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { getFirestoreOrderedNotifications } from '../../store/selectors/notifications';
+import moment from 'moment';
 
-export default function Notifications() {
+
+
+function Notifications({ notifications }) {
+  const NotificationsList = () => notifications.map(notification =>
+    <li
+      key={notification.id}>
+      <span className="green-text">
+        {notification.user}
+      </span>
+      <span>{` ${notification.content}`}</span>
+      <div className="grey-text note-time">
+        {moment(notification.time.toDate()).fromNow()}
+      </div>
+    </li>
+  );
+
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section" >
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <NotificationsList />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  notifications: getFirestoreOrderedNotifications(state)
+});
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'notifications' }
+  ])
+)(Notifications);
