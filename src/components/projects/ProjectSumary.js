418b713061d24f1af95381ab97fb23ff0ae12eeb
Ruy Mallo <ruymallo@gmail.com>
2019-03-31 22:03:09 -0300

    Add delete project button to ProjectSumary component and dispatch deleteFirebaseProject on click

--- src/components/projects/ProjectSumary.js
+++ src/components/projects/ProjectSumary.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import moment from 'moment';
+import { deleteFirebaseProject } from '../../store/actions/project'
 
-export default function ProjectSumary({project}) {
-  const { content, createdAt, title } = project;
+function ProjectSumary({ project, deleteFirebaseProject }) {
+  const { content, createdAt, title, id } = project;
+
+  const handleDelete = event => {
+    event.preventDefault();
+    event.stopPropagation();
+    deleteFirebaseProject(id);
+  }
 
   return (
     <div className="card z-depth-0 project-summary">
@@ -11,6 +19,17 @@ export default function ProjectSumary({project}) {
         <p>{content}</p>
         <p className="grey-text">{moment(createdAt.toDate()).calendar()}</p>
       </div>
+      <button
+        className="btn waves-effect waves-light red darken-1"
+        onClick={handleDelete} >
+          Delete
+      </button>
     </div>
   );
 }
+
+const mapDispatchToProps = {
+  deleteFirebaseProject
+}
+
+export default connect(null, mapDispatchToProps)(ProjectSumary);
