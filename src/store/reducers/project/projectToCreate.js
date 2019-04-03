9b936c1cb3480bf7699ca76946e6f7158c5a8509
Ruy Mallo <ruymallo@gmail.com>
2019-04-02 23:27:48 -0300

    Add login to projectToCreate reducer

--- src/store/reducers/project/projectToCreate.js
+++ src/store/reducers/project/projectToCreate.js
@@ -1,19 +1,45 @@
+import { SET_PROJECT_TO_CREATE_DATA } from '../../constants/actionTypes';
+
 const projectToCreateInitialState = {
-  id: null,
-  author: {
-    id: '',
-    firstName: '',
-    lastName: ''
-  },
   title: '',
   content: {
     lead: '',
     body: ''
   },
-  images: null,
-  createdAt: null
+  images: null
 };
 
 export default function projectToCreate(state = projectToCreateInitialState, action = {}) {
+  if (action.type === SET_PROJECT_TO_CREATE_DATA) {
+    const setDeepPath = () => {
+      const path = action.key.split('.');
+
+      return {
+        ...state,
+        [path[0]]: {
+          ...state.path[0],
+          [path[1]]: action.value
+        }
+      };
+    };
+
+    switch (action.key) {
+      case 'title':
+        return {
+          ...state,
+          title: action.value
+        };
+
+      case 'content.lead':
+      case 'content.body':
+      case 'images.intro':
+      case 'images.full':
+        return setDeepPath();
+
+      default:
+        return state;
+    }
+  }
+
   return state;
 }
